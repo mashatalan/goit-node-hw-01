5ed282c480b7b4b51834d53b7c839e972bcdc5d7
Maria Talan <mashenka.talan@gmail.com>
2023-08-29 00:36:12 +0300

    Updated dependencies, added cli support.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Command } = require('commander');
 const {
   listContacts,
   getContactById,
@@ -5,18 +6,49 @@ const {
   removeContact,
 } = require('./contacts');
 
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
+program.parse(process.argv);
+
+const argv = program.opts();
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list': {
+      const contactList = await listContacts();
+      console.dir(contactList);
+      break;
+    }
+
+    case 'get': {
+      const contact = await getContactById(id);
+      console.dir(contact);
+      break;
+    }
+
+    case 'add': {
+      const contact = await addContact(name, email, phone);
+      console.dir(contact);
+      break;
+    }
+
+    case 'remove': {
+      const contact = await removeContact(id);
+      console.dir(contact);
+      break;
+    }
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+// fix for top level await
 (async () => {
-  // const contacts = await listContacts();
-  // console.log(contacts);
-
-  // const contact = await getContactById('rsKkOQUi80UsgVPCcLZZW');
-  // console.log(contact);
-  //
-  // const contact = await removeContact('AeHIrLTr6JkxGE6SN-0Rw');
-  // console.log(contact);
-
-  // await addContact('Maria', 'mashenka@gmail.com', '096-817-42-49' );
-
+  await invokeAction(argv);
 })();
-
